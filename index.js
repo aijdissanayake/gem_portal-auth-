021215455a89bb9922435bc2f55359233bdabf89
Achala Dissanayake <aijdissanayake@gmail.com>
2017-03-30 05:33:39 +0530

    intializing models and db connections

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+// connect to the database and load models
+require('./server/models').connect(config.dbUri);
+
 //create express instance
 const app = express();
 //specify the port number
@@ -12,9 +18,24 @@ app.use(express.static('./client/dist/'));
 //parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// pass the passport middleware
+app.use(passport.initialize());
+
+// load passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
 // routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 
 // start the server
